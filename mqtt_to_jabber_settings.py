d0b30a7994fb6602135ff417533d02362f423b1b
Folkert van Heusden <mail@vanheusden.com>
2014-04-28 20:48:21 +0200

    More generic settings.

--- mqtt_to_jabber_settings.py
+++ mqtt_to_jabber_settings.py
@@ -3,18 +3,26 @@
 # (C) 2014 by folkert van heusden
 # mail@vanheusden.com
 
-xmpp_user_from = 'domotica@jabber.vanheusden.com'
-xmpp_user_password = 'jochie'
+# you might want to adjust the following parameters
+
+xmpp_user_from = 'sender@host.bla'
+xmpp_user_password = '****PASSWORD****'
 xmpp_connect_interval = 0.5
-xmpp_user_to = 'folkert@jabber.vanheusden.com'
+xmpp_user_to = 'youraccount@somehost.com'
+# shows a counter in the xmpp-status
 xmpp_heartbeat = False
 
-mqtt_server = 'test.mosquitto.org'
+mqtt_server = 'test.mosquitto.org' # point this to your local mqtt server
 mqtt_connect_interval = 0.5
-mqtt_topic = [ '#' ]
+mqtt_topic = [ '#' ] # topic(s) to listen to
 mqtt_topic_qos = 0
 
 verbose = True
 
-xmpp_mqtt_topic_regexp_patterns = [ 'text', 'sms' ]
-xmpp_mqtt_payload_regexp_patterns = [ '.*' ]
+# it works like this:
+# - IF one of the topics matches
+# - AND
+# - IF one of the payloads matches
+# - THEN send via jabber
+xmpp_mqtt_topic_regexp_patterns = [ 'text.*bla', 'sms[0-9]*' ] # regexps to look for
+xmpp_mqtt_payload_regexp_patterns = [ '.*' ] # regexps to look for
